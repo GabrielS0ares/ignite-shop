adf53aed5bb248d500711ff7d6253d0e99291841
GabrielS0ares <soaresgabriel33@gmail.com>
2023-05-24 13:11:55 -0300

    :lipstick: style: alterando a fonte do projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto_Flex } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const FontRoboto = Roboto_Flex({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={FontRoboto.className}>{children}</body>
     </html>
   )
 }
